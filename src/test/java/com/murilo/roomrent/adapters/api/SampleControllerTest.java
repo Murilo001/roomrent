fb2419a7468aed887323f3dfda8fa3ab042d8425
Murilo Oliveira <murilo001os@gmail.com>
2023-03-29 21:53:41 -0300

    Rest controller test sample

--- src/test/java/com/murilo/roomrent/adapters/api/SampleControllerTest.java
+++ src/test/java/com/murilo/roomrent/adapters/api/SampleControllerTest.java
@@ -1,18 +1,38 @@
 package com.murilo.roomrent.adapters.api;
 
+import com.murilo.roomrent.application.SampleDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.Is.is;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    private MockMvc mvc;
 
-    }
+    @MockBean
+    private SampleController sampleController;
 
     @Test
-    void shouldGetASamplePassingAValidPath() {
+    public void getSampleById() throws Exception {
+        String filter = "foo";
+        SampleDTO sampleDTO = new SampleDTO();
+        BDDMockito.given(sampleController.get(filter)).willReturn(sampleDTO);
 
+        mvc.perform(get("/sample/" + filter)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name", is(sampleDTO.name)));
     }
-
 }
